49899e1275be7afd0897092de77bd39120fcf0e8
Jiaxiu Li <57015757+jiaxiuli@users.noreply.github.com>
2020-04-27 17:08:10 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@
     $('a.page-scroll').bind('click', function(event) {
         var $ele = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($ele.attr('href')).offset().top - 60)
+            scrollTop: ($($ele.attr('href')).offset().top - 0)
         }, 1450, 'easeInOutExpo');
         event.preventDefault();
     });
